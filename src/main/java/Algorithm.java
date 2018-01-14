e6558b63554ee5ee1a0f20ec1c522ed6d274f51e
kira <kirakari1101@gmail.com>
2018-01-14 20:34:50 +0300

    visualization

--- src/main/java/Algorithm.java
+++ src/main/java/Algorithm.java
@@ -1,14 +1,15 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Map;
 import java.util.Scanner;
 
-public class Algorithm {
+public class Algorithm
+{
+    public static int speed = 500;
 
-    private ArrayList<ArrayList<Integer>> g = new ArrayList<>();
-    private ArrayList<Boolean> visited = new ArrayList<>();
-    private ArrayList<Integer> path = new ArrayList<>();
-    private ArrayList<Integer> optimal_path = new ArrayList<>();
+    private ArrayList<ArrayList<Integer>> g = new ArrayList<> ();
+    private ArrayList<Boolean> visited = new ArrayList<> ();
+    private ArrayList<Integer> path = new ArrayList<> ();
+    private ArrayList<Integer> optimal_path = new ArrayList<> ();
 
     private int n = 0;
     private int min_weight = 99999999;
@@ -17,35 +18,82 @@ public class Algorithm {
 
     private Visualization visual;
 
-    Algorithm(Visualization visual){
+    Algorithm(Visualization visual)
+    {
         this.visual = visual;
+    }
+
+    public void start()
+    {
+        getInput();
+        updateViz();
+        DFS(0, -1, n - 1, 0);
+        showAnswer();
+    }
+
+    private void getInput(){
+        System.out.println("Введите количество городов: ");
+        Scanner cin = new Scanner(System.in);
+        n = cin.nextInt();
+        System.out.println("Введите матрицу смежности: ");
+        for(int i = 0; i < n; i++) {
+            g.add(new ArrayList<> ());
+            path.add(-1);
+            visited.add(false);
+            for (int j = 0; j < n; j++) {
+                int k = cin.nextInt();
+                g.get(i).add(k);
+            }
+        }
+    }
+
+    private void updateViz()
+    {
+        ArrayList<Point> points = getCoordinates();
+        for(int i = 0; i < n; i++){
+            visual.addVertex(i, (int)(points.get(i).x * 250) + 250, (int)(points.get(i).y * 250) + 250);
+            visual.turnOffVertex(i);
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                if(i == j) continue;
+                visual.addEdge(i, j);
+                visual.turnOffEdge(i, j);
+            }
+        }
     }
 
     private void DFS(int v, int from, int steps, int weight)
     {
+
+
         if (visited.get(v))
         {
             return;
         }
 
+        turnOn(from, v);
+        visual.changeSumOfWeights(weight, min_weight);
 
-        if(min_weight < weight)
+        if(weight > min_weight)
         {
+            turnOff(from, v);
             return;
         }
 
+        visited.set(v, true);
+        path.set(v, from);
         if (steps == 0 && g.get(v).get(start)!= -1)
         {
             weight += g.get(v).get(start);
             if(weight < min_weight){
                 min_weight = weight;
                 optimal_path = (ArrayList<Integer>) path.clone();
+                finish = v;
+                visual.changeSumOfWeights(weight, min_weight);
             }
-            finish = v;
         }
-
-        visited.set(v, true);
-        path.set(v, from);
 
         for(int i = 0; i < n; i++)
         {
@@ -56,75 +104,70 @@ public class Algorithm {
             DFS(i, v, steps - 1, weight + g.get(v).get(i));
         }
         visited.set(v, false);
-    }
 
-
-    private ArrayList<Integer> get_path()
-    {
-        ArrayList<Integer> ans = new ArrayList<>();
-        for (int v = finish; v != start; v = optimal_path.get(v))
-        {
-            ans.add(v);
-        }
-        ans.add(start);
-        Collections.reverse(ans);
-        return ans;
-    }
-
-    public void start(){
-        getInput();
-        updateViz();
-        DFS(0, -1, n - 1, 0);
-        showAnswer();
-    }
-
-    private void updateViz(){
-        ArrayList<Point> points = getCoordinates();
-        for(int i = 0; i < n; i++){
-            visual.addVertex(i, (int)(points.get(i).x * 250) + 250, (int)(points.get(i).y * 250) + 250);
-        }
-
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                if(i == j) continue;
-                visual.addEdge(i, j, g.get(i).get(j), true);
-            }
-        }
-
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < i; j++){
-                visual.addEdge(i, j, g.get(i).get(j), false);
-            }
-        }
-    }
-
-    private void getInput(){
-        System.out.println("Введите количество городов: ");
-        Scanner cin = new Scanner(System.in);
-        n = cin.nextInt();
-        System.out.println("Введите матрицу смежности: ");
-        for(int i = 0; i < n; i++) {
-            g.add(new ArrayList<>());
-            path.add(-1);
-            visited.add(false);
-            for (int j = 0; j < n; j++) {
-                int k = cin.nextInt();
-                g.get(i).add(k);
-            }
-        }
+        turnOff(from, v);
     }
 
     private void showAnswer(){
         ArrayList<Integer> a = get_path();
         for(int i : a){
+            visual.log(i+" - ");
             System.out.print(i+" ");
         }
+        visual.log(start+"\nМинимальный вес "+min_weight+"\n");
         System.out.println("\n"+min_weight);
     }
 
+    private void turnOn(int from, int v){
+        visual.turnOnVertex(v);
+        visual.turnOnEdge(from, v);
+        waitSecond();
+    }
+
+    private void turnOff(int from, int v){
+        visual.turnOffVertex(v);
+        visual.turnOffEdge(from, v);
+        waitSecond();
+    }
+
+    private void waitSecond(){
+        try {
+            Thread.sleep(speed);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private ArrayList<Integer> get_path()
+    {
+        ArrayList<Integer> ans = new ArrayList<> ();
+
+        System.out.println(start+" "+finish);
+        int prev = -1;
+        for (int v = finish; v != start; v = optimal_path.get(v))
+        {
+            turnOn(prev, v);
+
+            ans.add(v);
+            prev = v;
+        }
+
+        ans.add(start);
+        Collections.reverse(ans);
+        visual.turnOnEdge(ans.get(0), ans.get(1));
+        for(int i = 0; i < n; i++){
+            if(i != 0 && i != n - 1){
+                visual.turnOnEdge(ans.get(i - 1), ans.get(i));
+            }
+            visual.turnOnVertex(ans.get(i));
+        }
+        visual.turnOnEdge(start, finish);
+        return ans;
+    }
+
     private ArrayList<Point> getCoordinates(){
-        ArrayList<Point> coord = new ArrayList<>();
+        ArrayList<Point> coord = new ArrayList<> ();
         double step = (2*Math.PI)/n;
         for(double i = 0; i < 2*Math.PI; i += step){
             double y = Math.sin(i);
@@ -134,58 +177,3 @@ public class Algorithm {
         return coord;
     }
 }
-
-
-
-/*
-TESTS:
-
- 3
- -1 50 100
- 999 -1 15
- 188 13 -1
-
- 5
- -1	34	76	45	49
- 64	-1	34	85	435
- 64	5	-1	9999	3
- 1	5	4	-1	8
- 45	9	1	5	-1
-
- 0 - 4 - 3 - 2 - 1
- 77
-
- 5
- -1	34	76	45	49
- 64	-1	0	85	435
- 64	5	-1	9999	0
- 0	5	4	-1	8
- 45	9	1	5	-1
-
- 0 - 4 - 3 - 2 - 1
- 39
-
- 6
- -1	34	76	45	49 3
- 64	-1	0	85	435 5
- 64	5	-1	9999	0 8
- 0	5	4	-1	8 5
- 45	9	1	5	-1 4
- 12 32 54 239 438 -1
-
- 0 - 5 - 1 - 2 - 4 - 3
- 40
-
- 7
- -1	34	76	45	49 3 5
- 64	-1	0	85	435 5 1
- 64	5	-1	9999	0 8 2
- 0	5	4	-1	8 5 5
- 45	9	1	5	-1 4 4
- 12 32 54 239 438 -1 2
- 456 7 4 7 4 9 -1
-
- 0 - 6 - 5 - 4 - 3 - 2 - 1
- 17
-
- */
--- src/main/java/Visualization.java
+++ src/main/java/Visualization.java
@@ -4,94 +4,220 @@ import com.mxgraph.view.mxGraph;
 import com.mxgraph.view.mxStylesheet;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Visualization extends JFrame {
-
+public class Visualization extends JFrame
+{
     private mxGraph graph;
     private Object parent;
+    private mxGraphComponent graphComponent;
+    private mxStylesheet stylesheet;
 
-    private ArrayList<Object> vertex = new ArrayList<>();
-    private ArrayList<ArrayList<Object>> edge = new ArrayList<>();
+    private JScrollPane scrollPane;
+    private JTextArea textArea;
+    private JSlider slider;
+    private JLabel sumWeightLabel;
 
-    private Map<String, Object> edgeStyle = new HashMap<String, Object>();
-    private Map<String, Object> vertexStyle = new HashMap<String, Object>();
+    private ArrayList<Object> vertex = new ArrayList<> ();
+    private ArrayList<ArrayList<Object>> edge = new ArrayList<> ();
 
-    Visualization(){
+    Visualization()
+    {
+        setLayout(new BorderLayout());
+
+        textArea = new JTextArea(20, 20); //размер панельки справа
+        textArea.setEditable(false);
+        scrollPane = new JScrollPane(textArea);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        getContentPane().add(scrollPane, BorderLayout.EAST);
+
+        sumWeightLabel = new JLabel("Сумма весов: ; Минимальная сумма весов: ");
+        getContentPane().add(sumWeightLabel, BorderLayout.SOUTH);
+
+        slider = new JSlider();
+        slider.setMinorTickSpacing(20);
+        slider.setPaintTicks(true);
+        slider.setValue(5);
+
+        slider.addChangeListener(e -> Algorithm.speed = slider.getValue()*100);
+        getContentPane().add(slider, BorderLayout.NORTH);
+
         graph = new mxGraph();
         parent = graph.getDefaultParent();
-        mxGraphComponent graphComponent = new mxGraphComponent(graph);
-        getContentPane().add(graphComponent);
-        mxStylesheet stylesheet = new mxStylesheet();
-        stylesheet.setDefaultEdgeStyle(edgeStyle);
-        stylesheet.setDefaultVertexStyle(vertexStyle);
+        graphComponent = new mxGraphComponent(graph); //чтобы прикрепить к jframe
+        getContentPane().add(graphComponent, BorderLayout.CENTER);
+        stylesheet = new mxStylesheet();
+        initVertexTurnOn();
+        initVertexTurnOff();
+        initEdgeTurnOn();
+        initEdgeTurnOff();
         graph.setStylesheet(stylesheet);
-
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(650, 650);
+        setSize(950, 650);
         setVisible(true);
 
-        for(int i = 0; i < 20; i++){
-            edge.add(new ArrayList<>());
-            for(int j = 0; j < 20; j++){
+        for(int i = 0; i < 20; i++)
+        {
+            edge.add(new ArrayList<> ());
+            for(int j = 0; j < 20; j++)
+            {
                 edge.get(i).add(null);
             }
         }
     }
 
-    public void addVertex(int number, int x, int y){
+    public void log(String s)
+    {
+        textArea.append(s);
+        textArea.setCaretPosition(textArea.getDocument().getLength()); //чтобы панелька справа спускалась вниз
+    }
+
+    public void changeSumOfWeights(int current, int min)
+    {
+        sumWeightLabel.setText("Сумма весов: "+current+"; Минимальная сумма весов: "+min);
+    }
+
+    public void addVertex(int number, int x, int y)
+    {
         beginUpdate();
         try{
-            //make default style
-            vertexStyle.put(mxConstants.STYLE_SHAPE,    mxConstants.SHAPE_CONNECTOR);
-            vertexStyle.put(mxConstants.STYLE_STROKECOLOR, "#000000");
-            vertexStyle.put(mxConstants.STYLE_FONTCOLOR, "#000000");
-            vertexStyle.put(mxConstants.STYLE_LABEL_BACKGROUNDCOLOR, "#ffffff");
-            vertex.add(graph.insertVertex(parent, null, Integer.toString(number), x, y, 50, 50,
-                    "defaultVertex;fillColor=lightgrey"));
+            vertex.add(graph.insertVertex(parent, null, Integer.toString(number), x, y, 50, 50));
+            System.out.println(number+" vertex has been added");
+            log(number+" вершина была добавлена\n");
         }
         finally {
             endUpdate();
         }
     }
 
-    public void addEdge(int v1, int v2, int weight, boolean align){ //align=1 => right
+    public void addEdge(int v1, int v2)
+    {
         beginUpdate();
         try{
-            edgeStyle.put(mxConstants.STYLE_SHAPE,    mxConstants.SHAPE_CONNECTOR);
-            edgeStyle.put(mxConstants.STYLE_ENDARROW, mxConstants.ARROW_CLASSIC);
-            edgeStyle.put(mxConstants.STYLE_STROKECOLOR, "#000000");
-            edgeStyle.put(mxConstants.STYLE_FONTCOLOR, "#000000");
-            edgeStyle.put(mxConstants.STYLE_LABEL_BACKGROUNDCOLOR, "#ffffff");
-
-            if(align){
-                edgeStyle.put(mxConstants.STYLE_ALIGN, "right");
-            } else {
-                edgeStyle.put(mxConstants.STYLE_ALIGN, "left");
-            }
-            edge.get(v1).set(v2, graph.insertEdge(parent, null, Integer.toString(weight), vertex.get(v1), vertex.get(v2)));
+            edge.get(v1).set(v2, graph.insertEdge(parent, null, "", vertex.get(v1), vertex.get(v2)));
         }
         finally {
             endUpdate();
         }
     }
 
-    public void turnOnVertex(int v){
-
+    public void turnOnVertex(int v)
+    {
+        System.out.println("Turn on "+v+" vertex");
+        log("Пришли в "+v+" вершину\n");
+        graph.setCellStyle("turnVertexOn", new Object[]{vertex.get(v)});
+        graphComponent.refresh();
     }
 
-    public void turnOffVertex(int v){
-
+    public void turnOffVertex(int v)
+    {
+        System.out.println("Turn off "+v+" vertex");
+        log("Вышли из "+v+" вершины\n");
+        graph.setCellStyle("turnVertexOff", new Object[]{vertex.get(v)});
+        graphComponent.refresh();
     }
 
-    private void beginUpdate(){
+    public void turnOnEdge(int v1, int v2)
+    {
+        if(v1 == -1) return;
+
+        if(v1 > v2)
+        {
+            int b = v1;
+            v1 = v2;
+            v2 = b;
+        }
+
+        System.out.println("Turn on from "+v1+" to "+v2);
+        graph.setCellStyle("turnEdgeOn", new Object[]{edge.get(v1).get(v2)});
+        graphComponent.refresh();
+    }
+
+    public void turnOffEdge(int v1, int v2)
+    {
+        if(v1 == -1) return;
+
+        if(v1 > v2)
+        {
+            int b = v1;
+            v1 = v2;
+            v2 = b;
+        }
+
+        System.out.println("Turn off from "+v1+" to "+v2);
+        graph.setCellStyle("turnEdgeOff", new Object[]{edge.get(v1).get(v2)});
+        graphComponent.refresh();
+    }
+
+    private void beginUpdate()
+    {
         graph.getModel().beginUpdate();
     }
 
-    private void endUpdate(){
+    private void endUpdate()
+    {
         graph.getModel().endUpdate();
     }
 
+    // ниже играюсь со шрифтами
+    private void initVertexTurnOn()
+    {
+        Map<String, Object> vertexStyle = new HashMap<> ();
+        vertexStyle.put(mxConstants.STYLE_VERTICAL_ALIGN, mxConstants.ALIGN_MIDDLE);
+        vertexStyle.put(mxConstants.STYLE_PERIMETER, mxConstants.PERIMETER_ELLIPSE);
+        vertexStyle.put(mxConstants.STYLE_PERIMETER_SPACING, 10);
+        vertexStyle.put(mxConstants.STYLE_SHAPE, mxConstants.SHAPE_ELLIPSE);
+        vertexStyle.put(mxConstants.STYLE_RESIZABLE, 0);
+        vertexStyle.put(mxConstants.STYLE_MOVABLE, 0);
+        vertexStyle.put(mxConstants.STYLE_FONTCOLOR, "#000000");
+        vertexStyle.put(mxConstants.STYLE_LABEL_BACKGROUNDCOLOR, "#ffffff");
+        vertexStyle.put(mxConstants.STYLE_FILLCOLOR, "#ff0000");
+        stylesheet.putCellStyle("turnVertexOn", vertexStyle);
+    }
+
+    private void initVertexTurnOff()
+    {
+        Map<String, Object> vertexStyle = new HashMap<> ();
+        vertexStyle.put(mxConstants.STYLE_VERTICAL_ALIGN, mxConstants.ALIGN_MIDDLE);
+        vertexStyle.put(mxConstants.STYLE_PERIMETER, mxConstants.PERIMETER_ELLIPSE);
+        vertexStyle.put(mxConstants.STYLE_PERIMETER_SPACING, 10);
+        vertexStyle.put(mxConstants.STYLE_SHAPE, mxConstants.SHAPE_ELLIPSE);
+        vertexStyle.put(mxConstants.STYLE_RESIZABLE, 0);
+        vertexStyle.put(mxConstants.STYLE_MOVABLE, 0);
+        vertexStyle.put(mxConstants.STYLE_FONTCOLOR, "#000000");
+        vertexStyle.put(mxConstants.STYLE_LABEL_BACKGROUNDCOLOR, "#ffffff");
+        vertexStyle.put(mxConstants.STYLE_FILLCOLOR, "#A9A9A9");
+        stylesheet.putCellStyle("turnVertexOff", vertexStyle);
+        stylesheet.setDefaultVertexStyle(vertexStyle);
+    }
+
+    private void initEdgeTurnOn()
+    {
+        Map<String, Object> edgeStyle = new HashMap<> ();
+        edgeStyle.put(mxConstants.STYLE_MOVABLE, 0);
+        edgeStyle.put(mxConstants.STYLE_RESIZABLE, 0);
+        edgeStyle.put(mxConstants.STYLE_SHAPE,    mxConstants.SHAPE_CONNECTOR);
+        edgeStyle.put(mxConstants.STYLE_ENDARROW, mxConstants.NONE);
+        edgeStyle.put(mxConstants.STYLE_STROKECOLOR, "#ff0000");
+        edgeStyle.put(mxConstants.STYLE_FONTCOLOR, "#000000");
+        edgeStyle.put(mxConstants.STYLE_LABEL_BACKGROUNDCOLOR, "#ffffff");
+        edgeStyle.put(mxConstants.STYLE_FILLCOLOR, "#ffffff");
+        stylesheet.putCellStyle("turnEdgeOn", edgeStyle);
+    }
+
+    private void initEdgeTurnOff()
+    {
+        Map<String, Object> edgeStyle = new HashMap<> ();
+        edgeStyle.put(mxConstants.STYLE_MOVABLE, 0);
+        edgeStyle.put(mxConstants.STYLE_RESIZABLE, 0);
+        edgeStyle.put(mxConstants.STYLE_SHAPE,    mxConstants.SHAPE_CONNECTOR);
+        edgeStyle.put(mxConstants.STYLE_ENDARROW, mxConstants.NONE);
+        edgeStyle.put(mxConstants.STYLE_STROKECOLOR, "#000000");
+        edgeStyle.put(mxConstants.STYLE_FONTCOLOR, "#000000");
+        stylesheet.putCellStyle("turnEdgeOff", edgeStyle);
+        stylesheet.setDefaultEdgeStyle(edgeStyle);
+    }
 }
